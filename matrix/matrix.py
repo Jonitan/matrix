4f3ab129d3d2dddb81508808841601b7dd71e32a
Jonitan <skiba8150@gmail.com>
2020-10-23 17:42:06 +0300

    Moved init check to post_init.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -10,8 +10,10 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Matrix():
 
-    def __init__(self, matrix: tuple = ()):
-        for row in matrix:
+    matrix: tuple = ()
+
+    def __post_init__(self):
+        for row in self.matrix:
             if type(row) is not tuple:
                 raise(BaseException(
                     "Row not represented by a tuple!"))
@@ -19,10 +21,10 @@ class Matrix():
                 if type(number) is not int:
                     raise(BaseException(
                         "Row item not represented by a number!"))
-            if len(row) != len(matrix):
+            if len(row) != len(self.matrix):
                 raise(BaseException(
                     "Number of rows different from number of columns!"))
-        object.__setattr__(self, "matrix", matrix)
+        object.__setattr__(self, "matrix", self.matrix)
 
     def __str__(self):
         return "%s" % (self.matrix,)
@@ -32,9 +34,6 @@ class Matrix():
 
 
 # Temporery testing section.
-# a = Matrix(((1,),))
-# print(a.__str__())
-# print(a.__repr__())
-
-# d = ()
-# print(str(len(d)))
+a = Matrix(((1, 3), (2, 2)))
+print(a.__str__())
+print(a.__repr__())
