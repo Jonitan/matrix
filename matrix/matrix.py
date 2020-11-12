09f665771f7162acd21115027f1319625a1a882b
Jonitan <skiba8150@gmail.com>
2020-11-12 22:27:24 +0200

    Refactor. Update version.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,45 +1,56 @@
-from dataclasses import dataclass
-
-# TODO:
-#   Add documentation.
+from typing import Union
 
 
-class Matrix(tuple):
+class Matrix:
 
     def __init__(self, matrix: tuple):
-        if Matrix.is_valid_matrix(matrix):
-            self.matrix = matrix
+        Matrix.is_valid_matrix(matrix)
+        self.matrix = matrix
 
-    def is_matrix(func):
-        def wrapper(*xs):
-            if type(xs[1]) is not Matrix:
-                raise TypeError("Argument should be of Matrix type.")
+    @property
+    def tuples(self):
+        return self.matrix
 
-            return func(*xs)
-        return wrapper
+    def __str__(self):
+        return str(self.matrix)
 
-    def is_int(func):
-        def wrapper(*xs):
-            if type(xs[1]) is not int:
-                raise TypeError("Argument should be of int type.")
-
-            return func(*xs)
-        return wrapper
+    def __repr__(self):
+        return f"Matrix({self.matrix})"
 
     def __len__(self):
         return len(self.matrix)
 
-    def __str__(self):
-        return "%s" % (self.matrix,)
+    def __getitem__(self, i):
+        return self.matrix[i]
 
-    def __repr__(self):
-        return "Matrix(%s)" % (self.matrix,)
+    def __hash__(self):
+        return hash(row for row in self)
 
     def __neg__(self):
         return Matrix(tuple(tuple(-item for item in row) for row in self))
 
-    @is_matrix
+    def __eq__(self, other):
+        if type(other) is not Matrix:
+            raise TypeError("Argument is not of Matrix type.")
+
+        if len(self) != len(other):
+            return False
+
+        if self is other:
+            return True
+
+        if any(s_row != o_row for s_row, o_row in zip(self, other)):
+            return False
+
+        return True
+
+    def __ne__(self, other):
+        return not self == other
+
     def __add__(self, other):
+        if type(other) is not Matrix:
+            raise TypeError("Argument is not of Matrix type.")
+
         if len(self) != len(other):
             raise ValueError("Matrix must be of the same order.")
 
@@ -48,6 +59,12 @@ class Matrix(tuple):
 
         return Matrix(tuple(tuple((sum(cell_pair) for cell_pair in zip(s_row, o_row)))
                             for s_row, o_row in zip(self, other)))
+
+    def __sub__(self, other):
+        if type(other) is not Matrix:
+            raise TypeError("Argument is not of Matrix type.")
+
+        return self + (-other)
 
     def __mul__(self, other):
         if type(other) is int or type(other) is float:
@@ -66,42 +83,8 @@ class Matrix(tuple):
     def __rmul__(self, other):
         return self * other
 
-    @is_matrix
-    def __sub__(self, other):
-        return self + (-other)
-
-    def __truediv__(self, other):
-        if type(other) is not int and type(other) is not float:
-            raise TypeError("Argument should be of int or float type.")
-
+    def __truediv__(self, other: Union[int, float]):
         return Matrix(tuple(tuple(cell / other for cell in row) for row in self))
-
-    @is_matrix
-    def __eq__(self, other):
-        if len(self) != len(other):
-            return False
-
-        if self is other:
-            return True
-
-        if any(s_row != o_row for s_row, o_row in zip(self, other)):
-            return False
-
-        return True
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __getitem__(self):
-        for row in self.matrix:
-            yield row
-
-    def __hash__(self):
-        return hash(row for row in self)
-
-    @property
-    def tuples(self):
-        print("%s" % (self.matrix,))
 
     @classmethod
     def is_valid_matrix(cls, matrix: tuple):
@@ -114,10 +97,7 @@ class Matrix(tuple):
         if any(len(matrix) != len(row) for row in matrix):
             raise ValueError("Number of rows and columns should be the same")
 
-        return True
-
     @classmethod
-    @is_int
     def unity(cls, number: int = 0):
         if number < 0:
             raise ValueError("Argument should be of positive value.")
@@ -125,7 +105,6 @@ class Matrix(tuple):
         return cls(tuple(tuple(1 if i == j else 0 for i in range(number)) for j in range(number)))
 
     @classmethod
-    @is_int
     def ones(cls, number: int = 0):
         if number < 0:
             raise ValueError("Argument should be of positive value.")
@@ -134,27 +113,25 @@ class Matrix(tuple):
 
 
 # Temporery testing section:
-a = Matrix(((1, 1, 1), (2, 2, 2), (3, 3, 3)))
-b = Matrix(((4, 4, 4), (5, 5, 5), (6, 6, 6)))
-c = Matrix(((1, 1, 1), (2, 3, 1), (0, 2, 0)))
+
+# a = Matrix(((1, 1, 1), (2, 2, 2), (3, 3, 3)))
+# b = Matrix(((4, 4, 4), (5, 5, 5), (6, 6, 6)))
+# c = Matrix(((1, 1, 1), (2, 3, 1), (0, 2, 0)))
 
 # print(a)
-#
 # print(repr(a))
-#
 # a.tuples
-#
+
 # print(Matrix.unity(3))
-#
 # print(Matrix.ones(3))
-#
+
 # print(a + b)
 # print(a + c)
 # print(b + a)
 # print(c + a)
 # print(a + a)
 # print(a + Matrix.unity(3))
-# print(a + ((1,),))
+# # print(a + ((1,),))
 
 # print(a - b)
 # print(b - a)
@@ -162,7 +139,7 @@ c = Matrix(((1, 1, 1), (2, 3, 1), (0, 2, 0)))
 # print(c - a)
 # print(a - a)
 # print(a - Matrix.unity(3))
-# print(a - ((1,),))
+# # print(a - ((1,),))
 
 # print(10 * a)
 # print(10.1 * a)
@@ -171,8 +148,8 @@ c = Matrix(((1, 1, 1), (2, 3, 1), (0, 2, 0)))
 # print(a / 10)
 # print(a / 10.1)
 
-a = Matrix(((1, 2), (3, 4)))
-b = Matrix(((3, 5), (6, 8)))
+# a = Matrix(((1, 2), (3, 4)))
+# b = Matrix(((3, 5), (6, 8)))
 
 # print(a * b)
 # print(b * a)
@@ -182,6 +159,8 @@ b = Matrix(((3, 5), (6, 8)))
 # print(a != b)
 # print(a == a)
 # print(a != a)
+# print(a == 1)
+# print(a != 1)
 
 # b = Matrix(((1, 2), (3, 4)))
 # print(a == b)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jonitan-matrix",
-    version="1.0.3",
+    version="1.0.4",
     author="Yonatan Naisteter",
     author_email="skiba8150@gmail.com",
     description="Matrix of size NxN implementation.",
